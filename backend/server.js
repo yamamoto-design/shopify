56b7c829b01e56d1e8c6096894b51e6f733ba918
Haruto <superDevNinja@outlook.com>
2025-04-26 11:15:00 +0900

    fix

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const port = 5000;
-
+const path = require("path");
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), "frontend", "dist")));
@@ -18,7 +18,6 @@ app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-const path = require("path");
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
