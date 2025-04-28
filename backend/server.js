5e2b262fba335f65a24f3ee4cb1ef4609f405a8e
Haruto <superDevNinja@outlook.com>
2025-04-28 20:41:07 +0900

    Oauth2

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,35 @@ app.use(express.static(path.join(process.cwd(), "frontend", "dist")));
 // Review route that uses the reviewController
 app.get("/api/reviews", reviewController.getReviews);
 
+app.post("/auth/callback", async (req, res) => {
+  const { code, state } = req.body;
+
+  const client_id = process.env.SHOPIFY_API_KEY;
+  const client_secret = process.env.SHOPIFY_API_SECRET;
+  const redirect_uri = process.env.SHOPIFY_APP_URL + "/auth/callback";
+
+  try {
+    const response = await axios.post(
+      `https://sentimenthug.myshopify.com/admin/oauth/access_token`,
+      {
+        client_id,
+        client_secret,
+        code,
+        redirect_uri,
+      }
+    );
+
+    // Extract the access token from the response
+    const accessToken = response.data.access_token;
+
+    // Send the access token to the frontend
+    res.json({ access_token: accessToken });
+  } catch (error) {
+    console.error("Error exchanging authorization code:", error);
+    res.status(500).json({ error: "Failed to exchange code for access token" });
+  }
+});
+
 app.listen(5000, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppProvider, Frame } from "@shopify/polaris";
+import { useAppBridge } from "@shopify/app-bridge-react";
 import "@shopify/polaris/build/esm/styles.css";
 import {
   BrowserRouter as Router,
@@ -10,17 +11,26 @@ import {
 import enTranslations from "@shopify/polaris/locales/en.json";
 import RealReviewComponent from "./components/RealReviewComponent";
 import GenerateReviewComponent from "./components/GenerateReviewComponent";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 import RepliBot from "./pages/RepliBot";
 import RealReview from "./pages/RealReview";
+import ShopifyLogin from "./pages/ShopifyLogin";
 
 function App() {
+  const shop = process.env.REACT_APP_SHOPIFY_SHOP_NAME;
+  const config = {
+    apiKey: process.env.SHOPIFY_API_KEY,
+    shopOrigin: `https://${shop}`, // e.g., "example.myshopify.com"
+    forceRedirect: true, // If you want to automatically redirect users to Shopify
+  };
+
   return (
     <AppProvider i18n={enTranslations}>
       <Router>
         <Frame>
           <Routes>
-            <Route path="/" element={<Navigate to="/republic" replace />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<ShopifyLogin />} />
             <Route path="/republic" element={<RepliBot />} />
             <Route path="/realreview" element={<RealReview />}>
               <Route path=":id" element={<RealReviewComponent />} />
